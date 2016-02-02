40af08c4e052022fb3d5066357943b508f53f225
Mauro de Lyon <maurodelyon@gmail.com>
2016-02-02 17:18:18 +0100

    7a met bug

--- Track1/B7a/main.c
+++ Track1/B7a/main.c
@@ -12,30 +12,42 @@ int main(void)
 {
 	/* Replace with your application code */
 	
+	
+	DDRC = 0b11111111;				// zet C naar output
+	DDRD = 0b00000000;				// zet D naar input
 	typedef enum {START,S1,S2,S3,END} STATE;
-	STATE state = STATE.START;
+	STATE state = START;
 
 	while (1)
 	{
 		switch (state)
 		{
 			case START:
-			if(PIND & BIT(6)) state = STATE.S1;
+				if(PIND & BIT(6)) state = S1;
+				PORTC = 0x1;
 			break;
 			case S1:
-			if(PIND & BIT(7) state = STATE.START;
-			if(PIND & BIT(5)) state = STATE.S2;
+				if(PIND & BIT(7)) state = START;
+				if(PIND & BIT(5)) state = S2;
+				PORTC = 0x2;
 			break;
 			case S2:
-			/* Your code here */
+				if(PIND & BIT(7)) state = START;
+				if(PIND & BIT(6)) state = S1;
+				if(PIND & BIT(5)) state = S3;
+				PORTC = 0x4;
 			break;
 			case S3:
-			/* Your code here */
+				if(PIND & BIT(7)) state = START;
+				if(PIND & BIT(5) || PIND & BIT(6)) state = END;
+				PORTC = 0x8;
 			break;
 			case END:
-			/* Your code here */
+				if(PIND & BIT(7)) state = START;
+				PORTC = 0x10;
 			break;
 		}
+		PIND = 0;
 	}
 }
 
