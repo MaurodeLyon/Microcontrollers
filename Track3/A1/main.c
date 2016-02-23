6050594bca2b306797f9d2fca92ddf7670064dd3
Mauro de Lyon <maurodelyon@gmail.com>
2016-02-23 10:34:20 +0100

    setup track 3

---
+++ Track3/A1/main.c
@@ -0,0 +1,18 @@
+/*
+ * A1.c
+ *
+ * Created: 23-2-2016 10:32:32
+ * Author : mauro
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
