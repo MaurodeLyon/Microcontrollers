278786cefb1c9c54eac06b8e9cdfe1cdb7b49106
Mauro de Lyon <maurodelyon@gmail.com>
2016-03-15 12:40:07 +0100

    start b3

---
+++ Track5/B3c/main.c
@@ -0,0 +1,18 @@
+/*
+ * B3c.c
+ *
+ * Created: 15-3-2016 12:30:48
+ * Author : mauro
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
