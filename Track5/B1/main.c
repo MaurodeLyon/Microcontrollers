8adfcd5bf99245370ea639326553362d7b260387
Hanburgah <arthur.brink1@gmail.com>
2016-03-15 10:26:04 +0100

    Track5 init

---
+++ Track5/B1/main.c
@@ -0,0 +1,18 @@
+/*
+ * B1.c
+ *
+ * Created: 15-3-2016 10:25:46
+ * Author : Arthur
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
