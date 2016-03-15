7554cdd604fb840104019081e77ce8c3903aa811
Hanburgah <arthur.brink1@gmail.com>
2016-03-15 11:58:25 +0100

    y u no work

---
+++ Track5/B2/main.c
@@ -0,0 +1,18 @@
+/*
+ * B2.c
+ *
+ * Created: 15-3-2016 11:10:29
+ * Author : Arthur
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
