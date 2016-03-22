9baaad9bb69181d24e644824f4bc19d29227e495
Mauro de Lyon <maurodelyon@gmail.com>
2016-03-22 17:09:57 +0100

    poging 1

--- Track5/B3c/main.c
+++ Track5/B3c/main.c
@@ -9,12 +9,10 @@
 #include <util/delay.h>
 
 #define BIT(x) ( 1<<x )
-#define DDR_SPI DDRB	// spi Data direction register
-#define PORT_SPI PORTB	// spi Output register
-#define SPI_SCK 1		// PB1: spi Pin System Clock
-#define SPI_MOSI 2		// PB2: spi Pin MOSI
-#define SPI_MISO 3		// PB3: spi Pin MISO
-#define SPI_SS 0		// PB0: spi Pin Slave Select
+#define DDR_I2C DDRB		// spi Data direction register
+#define PORT_I2C PORTB		// spi Output register
+#define I2C_SERIAL_DATA 0	// PB0: I2C Pin data
+#define I2C_SERIAL_CLOCK 1	// PB0: I2C Pin clock
 #define F_CPU 1000000UL
 
 // wait(): busy waiting for 'ms' millisecond - used library: util/delay.h
--- Track6/EindProject/GhettoGochi/main.c
+++ Track6/EindProject/GhettoGochi/main.c
@@ -8,14 +8,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "i2c_master.h"
+
 #define BIT(x) ( 1<<x )
-#define DDR_SPI DDRB	// spi Data direction register
-#define PORT_SPI PORTB	// spi Output register
-#define SPI_SCK 1		// PB1: spi Pin System Clock
-#define SPI_MOSI 2		// PB2: spi Pin MOSI
-#define SPI_MISO 3		// PB3: spi Pin MISO
-#define SPI_SS 0		// PB0: spi Pin Slave Select
-#define F_CPU 1000000UL
+#define I2C_SCL_PIN		0			// I2C Serial Clock
+#define I2C_SDA_PIN		0			// I2C Serial Data	
+#define DEVICE_ADDRES	0b11100000	// device addres
+#define SYSTEM_SETUP	0b00100001	// system setup
 
 int smile_bmp[] =
 {
@@ -53,14 +52,62 @@ int frown_bmp[] =
 	0b00111100
 };
 
+void init(void)
+{
+	i2c_init();
+	i2c_start(DEVICE_ADDRES);
+	//Internal system clock enable
+	i2c_write(SYSTEM_SETUP);
+	//ROW/INT output pin set + INT pin output level set
+	i2c_write(0b10101111);
+	//Dimming set
+	i2c_write(0b11101001);
+	//Blinking set
+	i2c_write(0b10000101);
+}
+
+void TWIInit(void)
+{
+	//set SCL to 400kHz
+	TWSR = 0x00;
+	TWBR = 0x0C;
+	//enable TWI
+	TWCR = (1<<TWEN);
+}
+
+void TWIStart(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	while ((TWCR & (1<<TWINT)) == 0);
+}
+//send stop signal
+void TWIStop(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+}
+
+void TWIWrite(uint8_t u8data)
+{
+	TWDR = u8data;
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while ((TWCR & (1<<TWINT)) == 0);
+}
+
 int main(void)
 {
 	DDRD = 0b11111111;
-	PORTD = 0b11111111;
+	TWIInit();
+	TWIStart();
+
 	/* Replace with your application code */
 	while (1)
 	{
-	
+		TWIWrite(0b01110000);
+		TWIWrite(0b00000000);
+		TWIWrite(0b00000000);
+		TWIWrite(0b00000000);
+		TWIWrite(0b00000000);
+		_delay_ms(1000);
 	}
 }
 
